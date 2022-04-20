3c1ab62860c64d367ba69343b80b408401f2ab88
Siu <siuc@protonmail.com>
2022-04-20 18:10:59 -0400

    setup db:reset command for server

---
+++ server/bin/resetdb.js
@@ -0,0 +1,54 @@
+// load .env data into process.env
+require("dotenv").config();
+
+// other dependencies
+const fs = require("fs");
+const { Client } = require("pg");
+const dbParams = require("../lib/db.js");
+const db = new Client(dbParams);
+
+// PG connection setup
+// const connectionString = process.env.DATABASE_URL ||
+//   `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+// const client = new Client();
+
+// Loads the schema files from db/schema
+const runSchemaFiles = async () => {
+  console.log(`-> Loading Schema Files ...`);
+  const schemaFilenames = fs.readdirSync("./db/schema");
+
+  for (const fn of schemaFilenames) {
+    const sql = fs.readFileSync(`./db/schema/${fn}`, "utf8");
+    console.log(`\t-> Running ${fn}`);
+    await db.query(sql);
+  }
+};
+
+const runSeedFiles = async () => {
+  console.log(`-> Loading Seeds ...`);
+  const schemaFilenames = fs.readdirSync("./db/seeds");
+
+  for (const fn of schemaFilenames) {
+    const sql = fs.readFileSync(`./db/seeds/${fn}`, "utf8");
+    console.log(`\t-> Running ${fn}`);
+    await db.query(sql);
+  }
+};
+
+const runResetDB = async () => {
+  try {
+    dbParams.host &&
+      console.log(`-> Connecting to PG on ${dbParams.host} as ${dbParams.user}...`);
+    dbParams.connectionString &&
+      console.log(`-> Connecting to PG with ${dbParams.connectionString}...`);
+    await db.connect();
+    await runSchemaFiles();
+    await runSeedFiles();
+    db.end();
+  } catch (err) {
+    console.error(`Failed due to error: ${err}`);
+    db.end();
+  }
+};
+
+runResetDB();
