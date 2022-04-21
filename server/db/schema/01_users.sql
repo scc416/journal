77e7217da82257bc299fd3797e814153052e5fa4
Siu <siuc@protonmail.com>
2022-04-21 14:28:57 -0400

    update db schema for saving mode

--- server/db/schema/01_users.sql
+++ server/db/schema/01_users.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username varchar UNIQUE NOT NULL,
-  password varchar NOT NULL
+  password varchar NOT NULL,
+  darkmode boolean NOT NULL DEFAULT TRUE
 );
