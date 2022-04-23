89d3618fc90e450844ee3bc09168173c84fd1abb
Siu <siuc@protonmail.com>
2022-04-22 21:24:58 -0400

    more error handling in the server for login/register

---
+++ server/constants.js
@@ -0,0 +1,7 @@
+export const ERROR_INCORRECT_CREDENTIALS = "Incorrect credentials.";
+
+export const ERROR_BLANK_PASSWORD = "Password can't be blank.";
+export const ERROR_BLANK_USERNAME = "Username can't be blank.";
+
+export const ERROR_PASSWORDS_NOT_MATCH = "Passwords do not match.";
+export const ERROR_USERNAME_ALREADY_TAKEN = "This username is already taken.";
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-
+const {
+  ERROR_INCORRECT_CREDENTIALS,
+  ERROR_BLANK_PASSWORD,
+  ERROR_BLANK_USERNAME,
+  ERROR_PASSWORDS_NOT_MATCH,
+  ERROR_USERNAME_ALREADY_TAKEN,
+} = require("../constants");
 const queryGenerator = require("../database/helpers/users");
 
 module.exports = (db) => {
@@ -9,17 +15,17 @@ module.exports = (db) => {
 
   router.post("/", async (req, res, next) => {
     const { username, password } = req.body;
-
     try {
+      if (!username) throw new Error(ERROR_BLANK_USERNAME);
+      if (!password) throw new Error(ERROR_BLANK_PASSWORD);
+
       const user = await getUserByValue("username", username);
       if (!user) {
-        throw new Error(
-          "Username doesn't exists. Please create a new account."
-        );
+        throw new Error(ERROR_INCORRECT_CREDENTIALS);
       }
 
       const correctPassword = await bcrypt.compare(password, user.password);
-      if (!correctPassword) throw new Error("Incorrect credentials.");
+      if (!correctPassword) throw new Error(ERROR_INCORRECT_CREDENTIALS);
 
       req.session.user_id = user.id;
       res.json(user.username);
@@ -32,13 +38,16 @@ module.exports = (db) => {
     const { username, password, confirmPassword } = req.body;
 
     try {
+      if (!username) throw new Error(ERROR_BLANK_USERNAME);
+      if (!password || !confirmPassword) throw new Error(ERROR_BLANK_PASSWORD);
+
       const passwordIsSame = confirmPassword === password;
-      if (!passwordIsSame) throw new Error("Passwords do not match.");
+      if (!passwordIsSame) throw new Error(ERROR_PASSWORDS_NOT_MATCH);
 
       const userWithSameUsername = await getUser(username);
 
       if (userWithSameUsername) {
-        throw new Error("This username is already taken.");
+        throw new Error(ERROR_USERNAME_ALREADY_TAKEN);
       }
 
       const hashedPassword = await bcrypt.hash(password, 12);
