7cfdf310b34190c3aba799ad75c4c7d3bd72f2c0
Siu <siuc@protonmail.com>
2022-04-23 00:25:07 -0400

    fix bug

--- client/src/features/mode/ToggleDarkModeButton.jsx
+++ client/src/features/mode/ToggleDarkModeButton.jsx
@@ -1,7 +1,4 @@
 import "./ToggleDarkModeButton.css";
-import Icon from "@mdi/react";
-import { mdiBrightness3 } from "@mdi/js";
-import { mdiBrightness7 } from "@mdi/js";
 import { Button } from "@blueprintjs/core";
 
 const ToggleDarkModeButton = ({ darkMode, toggleDarkMode }) => {
