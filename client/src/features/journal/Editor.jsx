2c908773d2a4b593b3b1584757782864eb4c168b
Siu <siuc@protonmail.com>
2022-04-23 20:25:20 -0400

    remove warnings

--- client/src/features/journal/Editor.jsx
+++ client/src/features/journal/Editor.jsx
@@ -1,9 +1,7 @@
 import { useState, useRef } from "react";
 import { Editor, EditorState } from "draft-js";
-import moment from "moment";
 import { saveJournal } from "./journalSlice";
 import { useDispatch } from "react-redux";
-import { getTodayDate } from "common/helpers";
 // import { convertFromRaw } from "draft-js";
 import { convertToRaw } from "draft-js";
 
@@ -22,7 +20,7 @@ const JournalEditor = ({ date }) => {
 
   const onChange = (state) => {
     const content = convertToRaw(state.getCurrentContent());
-    dispatch(saveJournal(content, getTodayDate()));
+    dispatch(saveJournal(content, date));
     setEditorState(state);
   };
 
