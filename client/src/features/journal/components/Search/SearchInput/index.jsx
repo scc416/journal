c27f8707e48825774b7ebce21e4db9c22fc35082
Siu <siuc@protonmail.com>
2022-04-25 02:29:13 -0400

    start mobile design

--- client/src/features/journal/components/Search/SearchInput/index.jsx
+++ client/src/features/journal/components/Search/SearchInput/index.jsx
@@ -1,17 +1,15 @@
-import { InputGroup, FormGroup } from "@blueprintjs/core";
+import { InputGroup } from "@blueprintjs/core";
 import CancelButton from "./CancelButton";
 
 const SearchInput = ({ search, changeHandler, cancel }) => {
   return (
-    <FormGroup>
-      <InputGroup
-        large={true}
-        placeholder="Search..."
-        value={search}
-        onChange={changeHandler}
-        rightElement={<CancelButton search={search} cancel={cancel} />}
-      />
-    </FormGroup>
+    <InputGroup
+      large={true}
+      placeholder="Search..."
+      value={search}
+      onChange={changeHandler}
+      rightElement={<CancelButton search={search} cancel={cancel} />}
+    />
   );
 };
 
